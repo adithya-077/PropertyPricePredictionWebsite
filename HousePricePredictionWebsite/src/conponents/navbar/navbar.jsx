11990c92cb1fe17e03c3a1bce863cba06813c91f
adithya <nadithya5636@gmail.com>
2023-03-15 06:48:19 +0530

    server changes

--- HousePricePredictionWebsite/src/conponents/navbar/navbar.jsx
+++ HousePricePredictionWebsite/src/conponents/navbar/navbar.jsx
@@ -14,9 +14,9 @@ function setTokenin(){
     <div className="navbar-main">
       <Link to='/' className="navbar-logo"><p>HP</p></Link>
       <div className="navbar-contents">
-        <Link to='/PredictResults'><p>Predict</p></Link>
-        <Link to='/About'><p>Hub</p></Link>
-        {cookie['token']==='null' && <Link to='/Login'><p>Login</p></Link>}
+        <Link to='/PredictResults'  className="predict-nav"><p>Predict</p></Link>
+        <Link to='/About' className="about-nav"><p>Hub</p></Link>
+        {cookie['token']==='null' && <Link to='/Login' className="loginbutton-nav"><p>Login</p></Link>}
         {cookie['token']!=='null' && <button onClick={setTokenin} className='logout-button'>logout</button>}
       </div>
      </div>
